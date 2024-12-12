df943a43bfd8e2958dfb7701bed6e31ebe0e420a
Socrates <socratessilva17@gmail.com>
2024-12-12 10:54:46 +0100

    MODIFICACION EN ECLIPSE

---
+++ src/HolaMundo/HolaMundo.java
@@ -0,0 +1,12 @@
+package HolaMundo;
+import java.util.*;
+public class HolaMundo {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Ingresa tu nombre: ");
+		String nombre = scanner.nextLine();
+		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
+		}
+/*MODIFICADO*/
+}
