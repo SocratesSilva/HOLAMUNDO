3d8af5c0b8ed48312878320aa802d7821bf526be
Socrates <socratessilva17@gmail.com>
2024-12-12 11:37:54 +0100

    Programa con comentarios Javadoc

--- src/HolaMundo/HolaMundo.java
+++ src/HolaMundo/HolaMundo.java
@@ -5,7 +5,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Ingresa tu nombre: ");
-		String nombre = scanner.nextLine();
+		String nombre = scanner.nextLine();//Solicita el nombre del usuario
 		System.out.println("¡Hola, " + nombre + "! Bienvenido a Java.");
 		System.out.println("Hola");
 		System.out.println("Cambio realizado después de hacer el pull");
@@ -14,4 +14,14 @@ public class HolaMundo {
 		/*MODIFICADO*/
 		/*Cambio realizado desde rama desarrollo*/
 		/*“Comentario después derealizar fusión”,*/
+	
+	/*Esto es Javadoc
+	 * 
+	 * a)Te da la bienvenida
+	 * 
+	 * 1.5.2
+	 * 
+	 * No lo se
+	 * 
+	 * */
 }
